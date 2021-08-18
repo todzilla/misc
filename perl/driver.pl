bbb9b9031d71d408fb730606cd1095d2427906e9
todzilla <fr33zone@gmail.com>
2021-08-17 20:07:42 -0400

    Add files via upload

---
+++ perl/driver.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+require plib;
+plib::testing();
+
+
+
+exit;
---
+++ perl/plib.pm
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+package plib;
+
+use strict;
+use warnings;
+
+sub testing {
+  print("Hello from plib, this is a test\n");
+
+return(0);
+}
+
+
+# This 1 is important in perl
+1
