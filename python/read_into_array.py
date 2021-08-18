09d270e62719c33985c1c650ead4807408799b3f
todzilla <fr33zone@gmail.com>
2021-08-18 12:42:38 -0400

    Add files via upload

---
+++ python/read_into_array.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# import
+import sys
+
+# var init
+#one=2
+one=sys.argv[1]
+
+# demonstrating conditionals 
+if one==1:
+  print("one")
+else:
+  print("not one but %i" %(one))
+
+
+# lists
+print("-------------------------------")
+ints = [0,1,2,3,4,5,6,7,8,9]
+ints.append(10)
+cnt = len(ints)
+print("Count = %i" %(cnt))
+ctr=0
+
+while(ctr < cnt):
+  print(ints[ctr])
+  ctr += 1
+
+# where sys import is required (though it happened automatically)
+# maybe version difference
+sys.exit("Done")
